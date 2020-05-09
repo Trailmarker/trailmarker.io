96c38f499dc18adc0f1619c456d55328bb7643e9
Tom Lynch <tom@trailmarker.io>
2020-05-09 22:37:19 +0930

    Correct deployment bug in theme pages

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
-import BannerLanding from '../components/BannerLanding'
+import BannerLanding from '../components/banner-landing'
 
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
